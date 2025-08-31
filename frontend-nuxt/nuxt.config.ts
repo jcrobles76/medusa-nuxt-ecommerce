6a6a78abd4ff8dfca2ba08ef5355849f2c9e8c23
jcrobles76 <jcrobles76@hotmail.com>
2025-09-01 01:14:06 +0200

    Initial commit of Medusa Nuxt E-commerce project

---
+++ frontend-nuxt/nuxt.config.ts
@@ -0,0 +1,9 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  runtimeConfig: {
+    public: {
+      medusaUrl: process.env.MEDUSA_URL || 'http://localhost:9000',
+    },
+  },
+})
